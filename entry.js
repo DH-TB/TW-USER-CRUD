353e97149715ce4cacfaed624ff566ffc92a0f54
DH-TB <929659475@qq.com>
2017-07-18 09:26:04 +0800

    modify folder name and add necessray file

--- entry.js
+++
@@ -1,9 +0,0 @@
-import React,{component} from 'react';
-import {render} from 'react-dom';
-
-class Hello extends React.Component{
-    render(){
-        return<div>Hello world</div>
-    }
-}
-render(<Hello />,document.getElementById('app'));
---
+++ public/src/components/add.js
@@ -0,0 +1,7 @@
+import React,{component} from 'react';
+
+export default class Add extends React.Component{
+    render(){
+        return<div>Hello world</div>
+    }
+}
