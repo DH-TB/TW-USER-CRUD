6fb3c61ed9599cc9f402257fc32e8b5697919232
DH-TB <929659475@qq.com>
2017-07-20 15:40:54 +0800

    refactor database option

--- routes/user.js
+++ routes/user.js
@@ -9,18 +9,18 @@ router.get('/getUser', (req, res, next)=> {
 router.get('/getOneUser/:username',(req,res,next)=>{
    userDao.queryOne(req,res,next);
 });
-router.delete('/deleteUser/:id',(req,res,next)=>{
-    userDao.deleteById(req,res,next);
-});
-router.post('/addUser',(req,res,next)=>{
-   userDao.add(req,res,next);
-});
 router.get('/getModifyUser/:id',(req,res,next)=>{
     userDao.queryById(req,res,next);
+});
+
+router.post('/addUser',(req,res,next)=>{
+    userDao.add(req,res,next);
+});
+router.delete('/deleteUser/:id',(req,res,next)=>{
+    userDao.deleteById(req,res,next);
 });
 router.put('/modifyUser',(req,res,next)=>{
     userDao.modify(req,res,next);
 });
-
 
 module.exports = router;
--- routes/userDao.js
+++ routes/userDao.js
@@ -6,48 +6,47 @@ const pool = mysql.createPool($conf.mysql);
 
 const jsonWrite = function (res, ret) {
     if (typeof ret === 'undefined') {
-        res.json({
+        res.send({
             code: '1',
             msg: '操作失败'
         });
-    } else {
-        res.json(ret);
+    }
+    else{
+        res.send(ret);
     }
 };
 
 module.exports = {
-    add: function (req, res, next) {
-        pool.getConnection(function (err, connection) {
+    add: (req, res, next)=> {
+        pool.getConnection((err, connection)=> {
             const body = req.body;
-            connection.query($sql.insert, [
-                body.username, body.name, body.age, body.sex,
-                body.phone, body.email, body.remark
-            ], (err, result)=> {
-                if (err) {
-                    return next(err);
-                }
-                if (result) {
-                    result = {
-                        code: 200,
-                        msg: '增加成功'
-                    };
-                }
-                jsonWrite(res, result);
-                connection.release();
-            });
+            connection.query($sql.insert, [body.username, body.name, body.age, body.sex, body.phone, body.email, body.remark],
+                (err, result)=> {
+                    if (err) {
+                        return next(err);
+                    }
+                    if (result) {
+                        result = {
+                            code: 200,
+                            msg: '增加成功'
+                        };
+                    }
+                    jsonWrite(res, result);
+                    connection.release();
+                });
         });
     },
-    modify: function (req, res, next) {
+    modify: (req, res, next)=> {
         const body = req.body;
         pool.getConnection((err, connection)=> {
-            connection.query($sql.update, [body.username,body.name,body.age,body.sex,body.phone,body.email,body.remark,body.id],(err, result)=> {
-                if(result.affectedRows > 0) {
+            connection.query($sql.update, [body.username, body.name, body.age, body.sex, body.phone, body.email, body.remark, body.id], (err, result)=> {
+                if (result.affectedRows > 0) {
                     result = {
                         code: 200,
                         msg: '修改成功'
                     };
-                   res.send(result);
                 }
+                jsonWrite(res, result);
                 connection.release();
             });
         });
@@ -56,6 +55,9 @@ module.exports = {
     queryAll: (req, res, next) => {
         pool.getConnection((err, connection)=> {
             connection.query($sql.queryAll, (err, result)=> {
+                if(err){
+                    return next(err);
+                }
                 jsonWrite(res, result);
                 connection.release();
             });
@@ -63,8 +65,11 @@ module.exports = {
     },
     queryOne: (req, res, next) => {
         const username = req.params.username;
-        pool.getConnection( (err, connection)=> {
+        pool.getConnection((err, connection)=> {
             connection.query($sql.queryOne, username, (err, result)=> {
+                if(err){
+                    return next(err);
+                }
                 jsonWrite(res, result);
                 connection.release();
             });
@@ -74,7 +79,10 @@ module.exports = {
         const id = req.params.id;
         pool.getConnection(function (err, connection) {
             connection.query($sql.queryById, id, (err, rows, result)=> {
-                for (var i in rows) {
+                if(err){
+                    return next(err);
+                }
+                for (const i in rows) {
                     jsonWrite(res, rows[i]);
                 }
                 connection.release();
@@ -85,6 +93,9 @@ module.exports = {
         pool.getConnection((err, connection) => {
             const id = req.params.id;
             connection.query($sql.delete, id, function (err, result) {
+                if(err){
+                    return next(err);
+                }
                 if (result.affectedRows > 0) {
                     result = {
                         code: 200,
