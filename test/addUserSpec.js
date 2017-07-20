a4e9aa181a7f4e230f94e1aa13c132a8414c7cd5
DH-TB <929659475@qq.com>
2017-07-20 20:00:48 +0800

    add user spec success

---
+++ test/addUserSpec.js
@@ -0,0 +1,34 @@
+require('../registerBabel');
+const server = require('../server');
+const request = require('supertest');
+const mysql = require('mysql');
+const $conf = require('../routes/conf');
+const $sql = require('../routes/userSqlMapping');
+const pool = mysql.createPool($conf.mysql);
+
+describe('unit test loading express', ()=> {
+    let id;
+    const userInfo = {
+        "username": 'huanglizhen',
+        "name": 'hhh',
+        "age": 21,
+        "sex": 'gril',
+        "phone": '15091671302',
+        "email": '929659475@qq.com',
+        "remark": 'dh'
+    };
+    afterEach((done)=> {
+        pool.getConnection((err, connection)=> {
+            connection.query('DELETE t.* FROM userInfo t INNER JOIN (SELECT MAX(id) maxt FROM userInfo) tmax ON t.id = tmax.maxt', (err, result)=> {
+                connection.release();
+                done();
+            });
+        });
+    });
+    it('response to /getOneUser', (done)=> {
+        request(server.listen())
+            .post('/addUser')
+            .send(userInfo)
+            .expect(200, {code: 200,msg: '增加成功'}, done);
+    })
+});
