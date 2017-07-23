971b5461c463bf94c1f814538ce2c24b5b0fa887
DH-TB <929659475@qq.com>
2017-07-23 22:00:32 +0800

    modify mysql password

--- routes/conf.js
+++ routes/conf.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "472561212",
+    password: "",
     port: 3306,
     database: 'UserManage'
 });
